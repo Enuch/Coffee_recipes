a6481df8d932f896b0bd5810277b24afc3845e41
Enuch <enuchsan@gmail.com>
2022-01-30 21:02:47 -0300

    Crud - done

--- app/src/main/java/com/example/coffeerecipes/fragments/list/HomeFragment.kt
+++ app/src/main/java/com/example/coffeerecipes/fragments/list/HomeFragment.kt
@@ -1,19 +1,21 @@
 package com.example.coffeerecipes.fragments.list
 
+import android.app.AlertDialog
 import android.os.Bundle
+import android.view.*
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.Navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 
 import com.example.coffeerecipes.R
 import com.example.coffeerecipes.viewModel.CoffeeViewModel
 import com.example.coffeerecipes.databinding.FragmentHomeBinding
+import com.example.coffeerecipes.model.Coffee
 
 class HomeFragment : Fragment() {
 
@@ -37,7 +39,32 @@ class HomeFragment : Fragment() {
             findNavController(it).navigate(R.id.action_homeFragment_to_registerFragment)
         }
 
+        setHasOptionsMenu(true)
+
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.delete_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.menu_delete){
+            deleteAll()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun deleteAll() {
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setPositiveButton("Yes") { _, _->
+            coffeeViewModel.deleteAll()
+            Toast.makeText(requireContext(), "Sucesso ao deletar todos!", Toast.LENGTH_SHORT).show()
+        }
+        builder.setNegativeButton("No") { _, _ -> }
+        builder.setTitle("Delete todos")
+        builder.setMessage("Tem certeza que deseja deletar tudo?")
+        builder.create().show()
     }
 
 }
